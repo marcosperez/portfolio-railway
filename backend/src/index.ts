72dacf1683b481188ac3c1c2c94cf081001f64f9
Marcos Daniel Perez <marcos.d.perez@gmail.com>
2022-07-30 12:20:39 -0300

    Creacion de docker compose local simil produccion

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,15 @@
 import { createApp } from "./app";
 
+const env = process.env.NODE_ENV;
 const port = process.env.PORT;
 const url = process.env.RAILWAY_STATIC_URL || "localhost";
 
 (async () => {
   const App = await createApp();
   App.listen(port, () => {
+    const protocole = env === "PROD" ? "https" : "http";
     console.log(
-      `⚡️[server]: Server is running at https://${url}:${port} ....`
+      `⚡️ [server][${env}]: Server is running at ${protocole}://${url}:${port} .... \u{1F37B}`
     );
   });
 })();
