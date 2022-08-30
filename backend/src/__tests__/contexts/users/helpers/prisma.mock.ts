4209f21485e848d6522e3e65e02412e7987412bb
Marcos Daniel Perez <marcos.d.perez@gmail.com>
2022-08-30 17:59:47 -0300

    fix prisma node module directory

--- backend/src/__tests__/contexts/users/helpers/prisma.mock.ts
+++ backend/src/__tests__/contexts/users/helpers/prisma.mock.ts
@@ -1,8 +1,7 @@
-import { PrismaClient as UserPrismaClient } from "../../../../../node_modules/@internal/prisma/users-client";
+import { PrismaClient as UserPrismaClient } from "@internal/prisma/users-client";
 import { mockDeep, mockReset, DeepMockProxy } from "jest-mock-extended";
 
 import usersUserPrismaClient from "../../../../contexts/users/infrastructure/repositories/prisma/UsersPrismaClient";
-import { iocContainer } from "../../../../inversify.config";
 
 jest.mock(
   "../../../../contexts/users/infrastructure/repositories/prisma/UsersPrismaClient",
@@ -11,13 +10,6 @@ jest.mock(
     default: mockDeep<UserPrismaClient>(),
   })
 );
-
-// beforeEach(() => {
-//   InversifyContainer.rebind<any>("UserPrismaClient").toDynamicValue(
-//     () => prismaMock
-//   );
-//   mockReset(prismaMock);
-// });
 
 export const prismaMock =
   usersUserPrismaClient as unknown as DeepMockProxy<UserPrismaClient>;
